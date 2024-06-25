9a3dce3fccf3f78f07c2ecd057dfdb6eb03c135f
FatimahAs <46461256+FatimahAs@users.noreply.github.com>
2024-06-25 08:05:55 +0300

    edit-4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./App";
 import { store } from "./services/store";
@@ -8,6 +9,7 @@ import { store } from "./services/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <Analytics />
       <App />
     </Provider>
   </React.StrictMode>
